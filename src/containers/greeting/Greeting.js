12b3dbd354e1591940f0c9af35a4d6369518ed86
Aryan Kushwaha <aryan.mnitj@gmail.com>
2024-09-04 17:44:02 +0530

    removed and added some changes as per preference

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -25,7 +25,6 @@ export default function Greeting() {
               >
                 {" "}
                 {greeting.title}{" "}
-                <span className="wave-emoji">{emoji("👋")}</span>
               </h1>
               <p
                 className={
@@ -38,7 +37,6 @@ export default function Greeting() {
               </p>
               <SocialMedia />
               <div className="button-greeting-div">
-                <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
                     href={require("./resume.pdf")}
@@ -50,16 +48,6 @@ export default function Greeting() {
                 )}
               </div>
             </div>
-          </div>
-          <div className="greeting-image-div">
-            {illustration.animated ? (
-              <DisplayLottie animationData={landingPerson} />
-            ) : (
-              <img
-                alt="man sitting on table"
-                src={require("../../assets/images/manOnTable.svg")}
-              ></img>
-            )}
           </div>
         </div>
       </div>
