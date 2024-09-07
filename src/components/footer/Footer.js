0ff176bd3cdaf3baf87e03cf1f30c1e9f56a7044
Aryan Kushwaha <aryan.mnitj@gmail.com>
2024-09-07 11:29:49 +0530

    added copyright in footer section

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,6 +9,8 @@ export default function Footer() {
   return (
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
+        <p className="footer-text">© Aryan's Portfolio
+        </p>
       </div>
     </Fade>
   );
